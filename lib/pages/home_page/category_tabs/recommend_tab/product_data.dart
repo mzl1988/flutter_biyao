3352c87b8811f42d7de682d976c2cd4bb34534dd
Michael Li <michael.li@arbaholdings.com.hk>
2019-07-25 15:45:16 +0800

    recommend tab

--- lib/pages/home_page/category_tabs/recommend_tab/product_data.dart
+++ lib/pages/home_page/category_tabs/recommend_tab/product_data.dart
@@ -1,7 +1,7 @@
 final List _products = [
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/16/1E/rBACW10kC9CAZQnxAABt8fcqNM8344.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -15,127 +15,7 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
-    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
-    "subtitle": "BCBG制造商直供",
-    "thirdContent": "40条好评",
-    "priceStr": "269",
-    "labels": [
-      {
-        "color": "primary",
-        "content": "精选"
-      },
-      {
-        "color": "danger",
-        "content": "一起拼"
-      }
-    ]
-  },
-  {
-    "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
-    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
-    "subtitle": "BCBG制造商直供",
-    "thirdContent": "40条好评",
-    "priceStr": "269",
-    "labels": [
-      {
-        "color": "danger",
-        "content": "一起拼"
-      }
-    ]
-  },
-  {
-    "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
-    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
-    "subtitle": "BCBG制造商直供",
-    "thirdContent": "40条好评",
-    "priceStr": "269",
-    "labels": [
-      {
-        "color": "primary",
-        "content": "爆品"
-      },
-      {
-        "color": "danger",
-        "content": "一起拼"
-      }
-    ]
-  },
-  {
-    "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
-    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
-    "subtitle": "BCBG制造商直供",
-    "thirdContent": "40条好评",
-    "priceStr": "269",
-    "labels": [
-      {
-        "color": "danger",
-        "content": "一起拼"
-      }
-    ]
-  },
-  {
-    "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
-    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
-    "subtitle": "BCBG制造商直供",
-    "thirdContent": "40条好评",
-    "priceStr": "269",
-    "labels": [
-      {
-        "color": "danger",
-        "content": "一起拼"
-      }
-    ]
-  },
-  {
-    "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
-    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
-    "subtitle": "BCBG制造商直供",
-    "thirdContent": "40条好评",
-    "priceStr": "269",
-    "labels": [
-      {
-        "color": "danger",
-        "content": "一起拼"
-      }
-    ]
-  },
-  {
-    "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
-    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
-    "subtitle": "BCBG制造商直供",
-    "thirdContent": "40条好评",
-    "priceStr": "269",
-    "labels": [
-      {
-        "color": "danger",
-        "content": "一起拼"
-      }
-    ]
-  },
-  {
-    "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
-    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
-    "subtitle": "BCBG制造商直供",
-    "thirdContent": "40条好评",
-    "priceStr": "269",
-    "labels": [
-      {
-        "color": "danger",
-        "content": "一起拼"
-      }
-    ]
-  },
-  {
-    "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/00/39/rBACVFkT4WaAc-osAAA2pTFtL70733.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -153,7 +33,7 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/01/F3/rBACW1kT4WeALek9AAAbiu6Mgmw235.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -167,7 +47,7 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/28/E7/rBACW1pSxHWAVImgAAAhF0KRC8g606.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -185,7 +65,7 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/3A/FD/rBACYV0xj-2AKYJuAACc6DERd-Y238.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -199,7 +79,7 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/74/A0/rBACYVv-nBuAOBJLAAA9yS90YvM795.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -213,7 +93,7 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/01/F2/rBACYVkT4WaAPH67AABCQA4OSIs840.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -227,7 +107,127 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/6B/15/rBACW1vlOj-Aa43sAABxqJSPXOk798.jpg",
+    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
+    "subtitle": "BCBG制造商直供",
+    "thirdContent": "40条好评",
+    "priceStr": "269",
+    "labels": [
+      {
+        "color": "danger",
+        "content": "一起拼"
+      }
+    ]
+  },
+  {
+    "image":
+        "https://bfs.biyao.com/group1/M00/7F/FC/rBACVFwbR--AHUYSAAB_zfTqZ4Q090.jpg",
+    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
+    "subtitle": "BCBG制造商直供",
+    "thirdContent": "40条好评",
+    "priceStr": "269",
+    "labels": [
+      {
+        "color": "danger",
+        "content": "一起拼"
+      }
+    ]
+  },
+  {
+    "image":
+        "https://bfs.biyao.com/group1/M00/15/0C/rBACVFlnFYiAIYtpAABGNDmMEJo355.jpg",
+    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
+    "subtitle": "BCBG制造商直供",
+    "thirdContent": "40条好评",
+    "priceStr": "269",
+    "labels": [
+      {
+        "color": "primary",
+        "content": "精选"
+      },
+      {
+        "color": "danger",
+        "content": "一起拼"
+      }
+    ]
+  },
+  {
+    "image":
+        "https://bfs.biyao.com/group1/M00/D7/97/rBACW1zb4WyANL2-AAB2B3FSXEE551.jpg",
+    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
+    "subtitle": "BCBG制造商直供",
+    "thirdContent": "40条好评",
+    "priceStr": "269",
+    "labels": [
+      {
+        "color": "danger",
+        "content": "一起拼"
+      }
+    ]
+  },
+  {
+    "image":
+        "https://bfs.biyao.com/group1/M00/F8/3F/rBACVF0J8reAAX97AABz3hk9Jww847.jpg",
+    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
+    "subtitle": "BCBG制造商直供",
+    "thirdContent": "40条好评",
+    "priceStr": "269",
+    "labels": [
+      {
+        "color": "primary",
+        "content": "爆品"
+      },
+      {
+        "color": "danger",
+        "content": "一起拼"
+      }
+    ]
+  },
+  {
+    "image":
+        "https://bfs.biyao.com/group1/M00/23/7D/rBACYVoXanSAcnUPAACaUk4qrPM608.jpg",
+    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
+    "subtitle": "BCBG制造商直供",
+    "thirdContent": "40条好评",
+    "priceStr": "269",
+    "labels": [
+      {
+        "color": "danger",
+        "content": "一起拼"
+      }
+    ]
+  },
+  {
+    "image":
+        "https://bfs.biyao.com/group1/M00/B1/F8/rBACYVydm9WAGJoHAAB4gR67bdw356.jpg",
+    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
+    "subtitle": "BCBG制造商直供",
+    "thirdContent": "40条好评",
+    "priceStr": "269",
+    "labels": [
+      {
+        "color": "danger",
+        "content": "一起拼"
+      }
+    ]
+  },
+  {
+    "image":
+        "https://bfs.biyao.com/group1/M00/7E/06/rBACVFwXSMWAQKMqAABIoaPiVtQ204.jpg",
+    "mainTitle": "纯棉DP免烫男士白短袖衬衫",
+    "subtitle": "BCBG制造商直供",
+    "thirdContent": "40条好评",
+    "priceStr": "269",
+    "labels": [
+      {
+        "color": "danger",
+        "content": "一起拼"
+      }
+    ]
+  },
+  {
+    "image":
+        "https://bfs.biyao.com/group1/M00/32/51/rBACVFrLNDeAZK8LAACeCjabHko161.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -241,7 +241,7 @@ final List _products = [
   },
    {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/08/F4/rBACW10fNQ6AfaXfAABb1kZUdW8025.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -255,7 +255,7 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/01/F3/rBACW1kT4WaAcV8OAAAUbTozx-I820.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -269,7 +269,7 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/21/31/rBACVFoOhyqAb-WbAACeRsmAWrY436.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
@@ -283,7 +283,7 @@ final List _products = [
   },
   {
     "image":
-        "https://bfs.biyao.com/group1/M00/B6/59/rBACVFyq5OaALFs2AABVN88XO_k413.jpg",
+        "https://bfs.biyao.com/group1/M00/01/F4/rBACYVkT4XCAI9dhAACCvwDSUbk634.jpg",
     "mainTitle": "纯棉DP免烫男士白短袖衬衫",
     "subtitle": "BCBG制造商直供",
     "thirdContent": "40条好评",
